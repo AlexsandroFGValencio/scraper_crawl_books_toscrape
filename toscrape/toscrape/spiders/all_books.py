7ea104e4abef175c7f4e9604d6b345f1d758fb5a
Alexsandro Valencio <alexsandrova@hotmail.com>
2020-11-10 16:36:08 -0300

    first commit

---
+++ toscrape/toscrape/spiders/all_books.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class AllBooksSpider(CrawlSpider):
+    name = 'all_books'
+    allowed_domains = ['books.toscrape.com']
+
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(url='http://books.toscrape.com/', headers={
+            'User-Agent': self.user_agent
+        })    
+
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths="//h3/a"), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths="//li[@class='next']/a"), process_request='set_user_agent'),
+    )
+
+    def set_user_agent(self, request, spider):
+        request.headers['User-Agent'] = self.user_agent
+        return request    
+
+    def parse_item(self, response):
+        yield {
+            'title': response.xpath("//div[@class='col-sm-6 product_main']/h1/text()").get(),
+            'price': (response.xpath("(//div[@class='col-sm-6 product_main']/p/text())[position()=1]").get()).strip(),
+            'book_url': response.urljoin(response.xpath("//h3/a").get()),
+        }
